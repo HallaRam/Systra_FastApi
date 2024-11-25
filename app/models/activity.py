3756a59423b39fdf32233169da4383d4d4c45578
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-11-25 15:10:02 +0100

    added comment

--- app/models/activity.py
+++ app/models/activity.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-# class for model in db 
+# class for model in database
 class Activity(Base):
     __tablename__ = "activities"
 
